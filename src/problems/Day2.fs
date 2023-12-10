5ea59b8fe2450b81abaf0351a7da63122731143a
Jonathon Choo <jonno.choo@gmail.com>
2023-12-10 23:15:41 +1100

    wip:

--- src/problems/Day2.fs
+++ src/problems/Day2.fs
@@ -4,15 +4,15 @@ module Day2 =
     open System.IO
     open System
 
-    type ColourCubeInfo = { Colour: string; Count: int }
-    type GameSet = { ColourCubes: ColourCubeInfo[] }
+    type ColourCube = { Colour: string; Count: int }
+    type GameSet = { ColourCubes: ColourCube[] }
 
     type Game =
         { Name: string
           Number: int
           GameSets: GameSet[] }
 
-    let parseColourCubeInfo (text: string) : ColourCubeInfo =
+    let parseColourCubeInfo (text: string) : ColourCube =
         let values = text.Trim().Split(' ') // Splits on space eg. 14 green
 
         { Colour = values[1]
@@ -26,7 +26,7 @@ module Day2 =
 
         { ColourCubes = colourCubes }
 
-    let parse (text: string) =
+    let parse (text: string) : Game =
         let init = text.Split(':') // Splits on : eg. Game 2: 6 blue, 3 green; 4 red, 1 green, 7 blue; 2 green
         let gameContent = init[1]
         let gameSetValues = gameContent.Split(';') // Splits on ; eg. 6 blue, 3 green; 4 red, 1 green, 7 blue; 2 green
@@ -44,10 +44,27 @@ module Day2 =
               Number = Int32.Parse(gameNameDetails[1])
               GameSets = gameSets }
 
-        printfn $"{game}"
-        text
+        game
+
+    let evaluateColourCube(colourCube: ColourCube) : bool =
+        if colourCube.Colour = "Red" && colourCube.Count > 12 then
+            false
+        else
+            true
+
+    let evaluateGameSet(gameSet: GameSet) : bool =
+        let validColourCubes = gameSet.ColourCubes |> Seq.filter (fun item -> evaluateColourCube item) |> Seq.toArray
+        gameSet.ColourCubes.Length = validColourCubes.Length
+
+    let evaluateGameIsInvalid(game: Game) : bool =
+        let validGameSets = game.GameSets
+            |> Seq.filter (fun item -> evaluateGameSet item)
+            |> Seq.toArray
+        game.GameSets.Length = validGameSets.Length
 
     let run =
         printfn "day 2"
         // parseGameSet "7 blue, 1 red, 14 green"
-        File.ReadAllLines("day-2.txt") |> Array.map (fun line -> parse line)
+        File.ReadAllLines("day-2.txt")
+        |> Seq.map (fun line -> parse line)
+        |> Seq.filter (fun item -> evaluateGameIsInvalid item)
